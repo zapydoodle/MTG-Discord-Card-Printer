b6c87a3bb95c9de07bb9a1bf0dd1ed70a909304f
zapydoodle <72476451+zapydoodle@users.noreply.github.com>
2024-06-27 23:27:43 +0100

    Disp;ay random card from Scryfall

--- Card_Gatherer/Card_Gatherer/Card_Gatherer.py
+++ Card_Gatherer/Card_Gatherer/Card_Gatherer.py
@@ -1,1 +1,7 @@
-
+import requests
+from PIL import Image
+from io import BytesIO
+url="https://api.scryfall.com/cards/random?format=image"
+response = requests.get(url)
+card=Image.open(BytesIO(response.content))
+card.show()
