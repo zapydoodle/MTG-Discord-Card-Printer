41f0a96ec219a7783778569936721922368fa683
zapydoodle <72476451+zapydoodle@users.noreply.github.com>
2024-06-28 00:05:07 +0100

    Changed Url Only Fetches card that are cards, no tokens, stickers ect.

--- Card_Gatherer/Card_Gatherer/Card_Gatherer.py
+++ Card_Gatherer/Card_Gatherer/Card_Gatherer.py
@@ -2,7 +2,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 def GetCard():
-    url="https://api.scryfall.com/cards/random?format=image"
+    url="https://api.scryfall.com/cards/random?format=image&q=t%3Asorcery+or+t%3Ainstant+or+t%3Acreature+or+t%3Aartifact+or+t%3Aenchantment+or+t%3Aplaneswalker+or+t%3Abattle"
     response = requests.get(url)
     card=Image.open(BytesIO(response.content))
     card.show()
